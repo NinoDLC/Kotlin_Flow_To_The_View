82602b0b5eeea9af75f4097110c8b41142f666c5
Nino <anthony@delcey.fr>
2021-11-17 12:31:20 +0100

    Remove unused POJO class

--- app/src/main/java/fr/delcey/pokedexfullflow/data/PokeApi.kt
+++ app/src/main/java/fr/delcey/pokedexfullflow/data/PokeApi.kt
@@ -1,14 +1,10 @@
 package fr.delcey.pokedexfullflow.data
 
 import fr.delcey.pokedexfullflow.data.pokemon.PokemonResponse
-import fr.delcey.pokedexfullflow.data.pokemonlist.PokemonListResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface PokeApi {
     @GET("pokemon/{pokemonId}/")
-    suspend fun getPokemonById(@Path("pokemonId") pokemonId : String) : PokemonResponse
-
-    @GET("pokemon")
-    suspend fun getAllPokemon() : PokemonListResponse
+    suspend fun getPokemonById(@Path("pokemonId") pokemonId: String): PokemonResponse
 }
