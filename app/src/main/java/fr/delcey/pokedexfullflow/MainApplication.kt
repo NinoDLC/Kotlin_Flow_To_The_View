bb07577c8cfce81d632e6f1293e16467ddb1d288
Nino <anthony@delcey.fr>
2021-11-18 11:15:24 +0100

    Simplify MainApplication

--- app/src/main/java/fr/delcey/pokedexfullflow/MainApplication.kt
+++ app/src/main/java/fr/delcey/pokedexfullflow/MainApplication.kt
@@ -4,14 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MainApplication : Application() {
-
-    companion object {
-        lateinit var instance: Application
-            private set
-    }
-
-    init {
-        instance = this
-    }
-}
+class MainApplication : Application()
